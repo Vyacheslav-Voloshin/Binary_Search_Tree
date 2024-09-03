7d2fb2572a5d4b76793b6455b633596403321b07
Vyacheslav-Voloshin <oks.volosh1na1981@gmail.com>
2024-09-03 20:12:43 +0300

    create static class Entry<T>

--- src/CustomTree.java
+++ src/CustomTree.java
@@ -1,14 +1,68 @@
 import java.io.Serializable;
 import java.util.AbstractList;
+import java.util.Collection;
+import java.util.List;
 
 public class CustomTree extends AbstractList<String> implements Serializable,Cloneable {
     @Override
-    public String get(int index) {
-        return null;
+    public String get(int index) throws UnsupportedOperationException {
+        throw  new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends String> c) {
+        throw  new UnsupportedOperationException();
+    }
+
+    @Override
+    protected void removeRange(int fromIndex, int toIndex) {
+        throw  new UnsupportedOperationException();
+    }
+
+    @Override
+    public List<String> subList(int fromIndex, int toIndex) {
+        throw  new UnsupportedOperationException();
+    }
+
+    @Override
+    public String set(int index, String element) {
+        throw  new UnsupportedOperationException();
+    }
+
+    @Override
+    public void add(int index, String element) {
+        throw  new UnsupportedOperationException();
+    }
+
+    @Override
+    public String remove(int index) {
+        throw  new UnsupportedOperationException();
     }
 
     @Override
     public int size() {
         return 0;
+    }
+
+    static  class Entry<T> implements Serializable{
+
+        String elementName;
+
+        boolean availableToAddLeftChildren;
+        boolean availableToAddRightChildren;
+
+        Entry<T> parent;
+        Entry<T> leftChild;
+        Entry<T> rightChild;
+
+        public Entry(String elementName) {
+            this.elementName = elementName;
+            this.availableToAddLeftChildren = true;
+            this.availableToAddRightChildren = true;
+        }
+
+        public boolean isAvailableToAddChildren(){
+            return this.availableToAddLeftChildren||this.availableToAddRightChildren;
+        }
     }
 }
