069f38712e7aa0c855c1279540aac75e26fd9787
Vyacheslav-Voloshin <oks.volosh1na1981@gmail.com>
2024-09-03 18:01:53 +0300

    create classes Solution and CustomTree

---
+++ src/CustomTree.java
@@ -0,0 +1,14 @@
+import java.io.Serializable;
+import java.util.AbstractList;
+
+public class CustomTree extends AbstractList<String> implements Serializable,Cloneable {
+    @Override
+    public String get(int index) {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
